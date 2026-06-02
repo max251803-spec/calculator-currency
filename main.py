6dab662eb18e8b7724fb1b842c8503119a855872
mvblinov <C:\Users\mvblinov\AppData\Roaming\The Bat! Pwd>
2026-06-02 13:33:22 +0400

    Добавил автоматическое получение курса валют

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("Привет, это мой калькулятор!")
+import requests
+response = requests.get("https://api.exchangerate-api.com/v4/latest/USD")
+data = response.json()
+kurs_rub = data["rates"]["RUB"]
