52e4b5e588300e0c0d529041e20d90f1becda3f4
mvblinov <C:\Users\mvblinov\AppData\Roaming\The Bat! Pwd>
2026-06-03 10:35:24 +0400

    Добавил округление и подключение request

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ import requests
 response = requests.get("https://api.exchangerate-api.com/v4/latest/USD")
 data = response.json()
 kurs_rub = data["rates"]["RUB"]
+rubles = float(input("Сколько ты хочешь перевести?"))*kurs_rub
+print("Итого:", round(rubles), "рублей")
